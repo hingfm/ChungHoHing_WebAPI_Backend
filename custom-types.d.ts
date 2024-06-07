c6608f2e841d83d7a9bb60437aa0a4e12d496484
hohing <hing.filemaker@gmail.com>
2024-06-07 16:02:16 +0800

    Add AI detect dog and cat breed function

---
+++ custom-types.d.ts
@@ -0,0 +1,6 @@
+// custom-types.d.ts
+declare module 'csv-parser' {
+    const content: any;
+    export default content;
+  }
+  
--- routes/uploads.ts
+++ routes/uploads.ts
@@ -1,8 +1,12 @@
 import Router, {RouterContext} from "koa-router";
 import koaBody from "koa-body";
 import mime from "mime-types";
-import { copyFileSync, existsSync, createReadStream } from "fs";
+import { copyFileSync, existsSync, createReadStream, readFileSync } from "fs";
 import { v4 as uuidv4 } from 'uuid';
+import * as tf from '@tensorflow/tfjs-node';
+import csv from 'csv-parser';
+import fs from 'fs';
+
 const upload_options= {
     multipart: true,
     formidable: { uploadDir: './img' }
@@ -11,96 +15,127 @@ const upload_options= {
 const koaBodyM = koaBody(upload_options);
 const fileStore:string= './img';
 const router:Router = new Router({ prefix:'/api/v1'});
+const modelPath = './tensorflow_model';
+const labelsPath = './tensorflow_model/petbreed_labels.csv';
+let labels: string[] = [];
+let model: any ;
 
-router.post('/images', koaBodyM, async (ctx: RouterContext, next: any) =>
-{
-try {   
+// Initialize labels and model
+async function init() {
+  labels = await loadLabels(labelsPath);
+  model = await loadModel();
+}
+
+function loadLabels(filePath: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+      const tempLabels: string[] = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data) => tempLabels.push(data.label))
+        .on('end', () => {
+          resolve(tempLabels);
+        })
+        .on('error', (error) => reject(error));
+  });
+}
+
+async function loadModel() {
+  if (!model) {
+        model = await tf.node.loadSavedModel(modelPath);
+  }
+  return model;
+}
+
+async function classifyImage(imagePath: string) {
+  await loadModel();
+  const imageBuffer = readFileSync(imagePath);
+  const decodedImage = tf.node.decodeImage(imageBuffer, 3); // Ensure this is a 3-channel (RGB) image
+  const resizedImage = tf.image.resizeBilinear(decodedImage, [300, 300]); // Resize to the expected dimensions
+  const normalizedImage = resizedImage.div(tf.scalar(255)); // Normalize pixel values
+  const batchedImage = normalizedImage.expandDims(0); // Add the batch dimension
+
+  if (!model) {
+      throw new Error("Model not loaded");
+  }
+
+  const prediction = await model.predict(batchedImage);
+  const predictionArray = await prediction.array();
+
+  const highestIndex = predictionArray[0].indexOf(Math.max(...predictionArray[0]));
+  const highestLabel = labels[highestIndex];
+
+  return highestLabel;
+}
+
+router.post('/images', koaBodyM, async (ctx: RouterContext) => {
+  try {
       const upload = ctx.request.files?.upload;
       let path: string | undefined;
       let name: string | undefined;
-      let type: string | undefined;;
-      let extension:string|null;
+      let type: string | undefined;
+
       if (Array.isArray(upload)) {
-          // Handle if 'upload' is an array of files
-          if (upload.length > 0) {
-              path = upload[0].filepath;
-              name = upload[0].newFilename;
-               type = upload[0].mimetype|| ''                   
-          }
+          path = upload[0]?.filepath;
+          name = upload[0]?.newFilename;
+          type = upload[0]?.mimetype || '';
       } else {
-          // Handle if 'upload' is a single file
           path = upload?.filepath;
           name = upload?.newFilename;
-          type = upload?.mimetype|| '';    
+          type = upload?.mimetype || '';
       }
-  if (type) {
-    extension = mime.extension(type) || null;
-  } else {
-    // Handle the case where 'type' is undefined
-    extension = null;
-  }
-  //const {path, name, type} = ctx.request.files.upload;
-  //const path:string|undefined = ctx.request.files?.upload?.filepath ;
-  //const name:string|undefined = ctx.request.files?.upload?.newFilename 
-  //const type:string |undefined= ctx.request.files?.upload?.mimetype;  
-  //const   extension = mime.extension(type)
 
-// console.log('Uploaded file details: '+JSON.stringify(ctx.request.files))
-// add some logging to help with troubleshooting
-      console.log('Uploaded file details:')
-      console.log(`path: ${path}`);
-      console.log(`filename: ${name}`);
-      console.log(`type: ${type}`);
-      console.log(`extension: ${extension}`);   
+      if (!path) {
+          throw new Error('No file uploaded.');
+      }
+
       const imageName = uuidv4();
       const newPath = `${fileStore}/${imageName}`;
-     if (path) {
-        copyFileSync(path, newPath);
-      } else {
-        throw new Error('Path is not defined for copying the file.');
-     }
+      copyFileSync(path, newPath);
+
+      const predictedLabel = await classifyImage(newPath);
+
+      console.log('Uploaded file details:', { path, name, type, predictedLabel });
       ctx.status = 201;
       ctx.body = {
-      filename: name,
-      type: type,
-      extension: extension,
-        links: {//`https://${ctx.host}${prefix}/${path}`
-
-            path: `http://${ctx.host}${router.url('get_image', imageName)}`
+          filename: name,
+          type: type,
+          predictedLabel,
+          links: {
+              path: `http://${ctx.host}${router.url('get_image', imageName)}`
           }
-
       };
-  } catch(err:any) {
-      console.log(`error ${err.message}`);
-      ctx.throw(500, 'upload error', {message: err.message});
+  } catch (err: any) {
+      console.error(`Error during file upload or classification: ${err.message}`);
+      ctx.throw(500, 'Upload or classification error', { message: err.message });
   }
 });
 
-router.get('get_image', '/images/:uuid([0-9a-f\\-]{36})', async (ctx: RouterContext, next: any) => {
+router.get('get_image', '/images/:uuid([0-9a-f\\-]{36})', async (ctx: RouterContext) => {
   const uuid = ctx.params.uuid;
   const path = `${fileStore}/${uuid}`;
-  console.log('client requested image with path', path);
+  console.log('Client requested image with path:', path);
   try {
-
-    // find the requested file and return it
       if (existsSync(path)) {
-        
-          console.log('image found');
+          console.log('Image found');
           const src = createReadStream(path);
           ctx.type = 'image/jpeg';
           ctx.body = src;
           ctx.status = 200;
       } else {
-          console.log('image not found');
+          console.log('Image not found');
           ctx.status = 404;
       }
-
-
-
-  } catch (err:any) {
-      console.log(`error ${err.message}`);
-      ctx.throw(500, 'image download error', {message: err.message});
+  } catch (err: any) {
+      console.error(`Error serving image: ${err.message}`);
+      ctx.throw(500, 'Image download error', { message: err.message });
   }
+});
+
+// Start the server with initialized data
+init().then(() => {
+  console.log('Labels and model loaded');
+}).catch((error) => {
+  console.error('Failed to initialize:', error);
 });
 
 export {router};
