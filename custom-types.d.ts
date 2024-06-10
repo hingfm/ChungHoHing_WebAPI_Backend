fc96e4406b5ed99b458e92509a06caf755f0c45f
hohing <hing.filemaker@gmail.com>
2024-06-11 07:26:26 +0800

    Add post twitter function

--- custom-types.d.ts
+++ custom-types.d.ts
@@ -1,6 +1,25 @@
 // custom-types.d.ts
-declare module 'csv-parser' {
-    const content: any;
-    export default content;
+
+declare module 'oauth' {
+  export class OAuth2 {
+      constructor(
+          clientId: string,
+          clientSecret: string,
+          baseSite: string,
+          authorizePath: string | null,
+          accessTokenPath: string,
+          customHeaders: any | null
+      );
+
+      getOAuthAccessToken(
+          extraParams: any,
+          params: { grant_type: string },
+          callback: (error: Error | null, accessToken: string | null, refreshToken?: string, results?: any) => void
+      ): void;
   }
-  
+}
+
+declare module 'csv-parser' {
+  const content: any;
+  export default content;
+}
